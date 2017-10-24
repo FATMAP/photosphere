493d89e9dbcc81a29a7ed9b55e4a45577cabbaf9
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2017-10-24 22:43:23 +0300

    Cleaned up

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 Photo sphere with three.js
 ===========================
 
-Read about it in <a href="http://blog.thematicmapping.org/2014/01/photo-spheres-with-threejs.html">this blog post</a>.
---
+++ js/CameraControls.js
@@ -0,0 +1,48 @@
+class CameraControls {
+	constructor(dom, camera, clickCallback) {
+		this.dom = dom;
+		this.camera = camera;
+		this.clickCallback = clickCallback;
+
+		dom.addEventListener( 'mousedown', this.onMouseDown.bind(this), false );
+		dom.addEventListener( 'mousemove', this.onMouseMove.bind(this), false );
+		dom.addEventListener( 'mouseup', this.onMouseUp.bind(this), false );
+
+		this.raycaster = new THREE.Raycaster();
+		this.mouse = false;
+
+		this.spin = 0;
+		this.tilt = Math.PI / 2;
+		this.zoom = 0;
+		this.position = new THREE.Vector3();
+		this.mousePos = new THREE.Vector2();
+	}
+
+	getForwardVector() {
+        let forward = new THREE.Vector3(0, 0, -1);
+        return forward.applyQuaternion(this.camera.quaternion);
+    }
+
+	update() {
+		this.camera.quaternion.setFromEuler(new THREE.Euler(this.tilt, 0, this.spin, "ZYX"));
+	}
+
+	onMouseDown(event) {
+		this.mouse = true;
+	}
+
+	onMouseUp(event) {
+		this.mouse = false;
+	}
+
+	onMouseMove(event) {
+		if (!this.mouse)
+			return;
+
+		this.mousePos.x = event.offsetX / this.dom.offsetWidth * 2 - 1;
+		this.mousePos.y = -(event.offsetY / this.dom.offsetWidth * 2 - 1);
+
+        this.raycaster.setFromCamera(this.mousePos, this.camera);
+        let direction = this.raycaster.ray.direction;
+	}
+}
